228d332e259dfc6ff8814581b2fb219e923c0f3a
Matteo Giantomassi <gmatteo@gmail.com>
2024-12-12 12:44:22 +0100

    Add gitignore, make plotting scripts executables, remove bkp files

--- Si_v_ZSISA_appriximation/plot_qha_approx.py
+++ Si_v_ZSISA_appriximation/plot_qha_approx.py
@@ -13,9 +13,9 @@ from abipy.dfpt.phonons import PhononBands
 from abipy.dfpt.qha_aproximation import QHA_App
 
 strains = [96,98,100,102,104,106]
-strains2 = [98,100,102,104,106] #EinfVib4(D) 
-#strains2 = [96,98,100,102,104] #EinfVib4(S) 
-#strains2 = [100,102,104] #EinfVib2(D) 
+strains2 = [98,100,102,104,106] #EinfVib4(D)
+#strains2 = [96,98,100,102,104] #EinfVib4(S)
+#strains2 = [100,102,104] #EinfVib2(D)
 
 gsr_paths = [os.path.join( "scale_{:d}_GSR.nc".format(s)) for s in strains]
 ddb_paths = [os.path.join( "scale_{:d}_GSR_DDB".format(s)) for s in strains]
@@ -25,7 +25,7 @@ dos_paths = [os.path.join( "scale_{:d}_PHDOS.nc".format(s)) for s in strains2]
 qha = QHA_App.from_files_app_ddb(ddb_paths, dos_paths)
 qha.plot_energies(tstop=800,tstart=0 ,num=11,title="Energies as a function of volume for different T")
 
-# Vinet 
+# Vinet
 qha.plot_vol_vs_t(tstop=800,tstart=0 ,num=101,title="Volume as a function of T")
 
 qha.plot_abc_vs_t(tstop=800,tstart=0 ,num=101,title="Lattice as a function of T")
